6be966cf239d8865760fd36ba71b07ce358def8e
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-01 15:11:34 +0200

    Vjezba 15

---
+++ ex15/src/index.js
@@ -0,0 +1,27 @@
+function main() {
+  // only change code below this line
+  var myNoun = "dog";
+  var myVerb = "ran";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks =
+    "My " +
+    myNoun +
+    " " +
+    myVerb +
+    " " +
+    "away" +
+    " " +
+    myAdverb +
+    " " +
+    "when he saw the" +
+    " " +
+    myAdjective +
+    ".";
+  (".");
+  // only change code above this line
+  return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
