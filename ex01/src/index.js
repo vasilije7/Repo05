b753029107f55150fc4aab4cb6d809e4bfb54e86
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-01 10:36:17 +0200

    Vjezba 1, varijable sa stringovima

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // only change code below this line
+  var myFirstName = "Vasilije";
+  var myLastName = "Vujovic";
+  var myHomeTown = "Podgorica";
+  // only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
