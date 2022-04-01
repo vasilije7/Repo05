433c95faa12ee65f9a9966eae980386c86df24b9
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-01 14:15:36 +0200

    Vjezba 8

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // only change code below this line
+  var myAdjective = "the best!";
+  var myString = "Mistral is ";
+  myString += myAdjective;
+  // only change code above this line
+  return myString;
+}
+
+console.log(main());
+module.exports = main;
