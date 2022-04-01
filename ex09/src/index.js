862f576e9c0e5421ee01d8c4a48935f321c5b278
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-01 14:22:08 +0200

    Deveta vjezba

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+  // only change code below this line
+  var lastName = "Doe";
+  var lastNameLenght = lastName.length;
+  // only change code above this line
+  return lastNameLenght;
+}
+
+console.log(main());
+module.exports = main;
