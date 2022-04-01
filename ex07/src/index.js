e0274238ab6f1aec9467c6e4990e165ddabb17cb
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-01 14:09:13 +0200

    Sedma vjezba

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+  // only change code below this line
+  var myName = "Vasilije";
+  var myString = "Hello, my name is " + myName + " and I am well!";
+  // only change code above this line
+  return myString;
+}
+
+console.log(main());
+module.exports = main;
